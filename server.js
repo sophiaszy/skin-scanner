e1db9497cb8c9640d34e01ddff561a8fee3d4f29
sophiashen <sophiashenziyi@gmail.com>
2021-02-14 01:53:46 -0800

    fixed frontend

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 
-const modelRouter = require('./Routes/handler');
+//const modelRouter = require('./Routes/handler');
 
 
 server.use(express.json());
 server.use(cors());
 server.use('/home',express.static('static'));
-server.use('/users', modelRouter);
+//server.use('/users', modelRouter);
 
 
 const PORT = process.env.PORT || 3000;
